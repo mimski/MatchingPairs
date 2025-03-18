ba6c357d6fd8be78a73a6543e98c04036a45199f
mimski <maria.bg.1989@gmail.com>
2025-03-18 17:50:48 +0200

    fixes a typo

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ loadCards();
 async function loadCards() {
   try {
       let response = await fetch("./data/card_info.json");
-      let cardsArray = await reponse.json();
+      let cardsArray = await response.json();
       console.log(cardsArray);
   } catch(error) {
       console.log(error);
